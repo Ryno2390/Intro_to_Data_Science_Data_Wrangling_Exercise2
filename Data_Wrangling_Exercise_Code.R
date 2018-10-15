4f312b68a631b7dbd826cee1ec00f9e83d5014d7
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-15 09:29:42 -0400

    5: Submit the project on Github (Include...the cleaned up data as a CSV file called titanic_clean.csv)

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -9,3 +9,9 @@ Data$embarked <- replace(Data$embarked, is.na(Data$embarked), "S")
 Data$age <- replace(Data$age, is.na(Data$age), mean(Data$age, na.rm = TRUE))
 
 Data$boat <- replace(Data$boat, is.na(Data$boat), "None")
+
+has_cabin_number <- replace(Data$cabin, is.na(Data$cabin), 0)
+has_cabin_number <- replace(has_cabin_number, has_cabin_number != 0, 1)
+Data$has_cabin_number <- has_cabin_number
+
+write.csv(Data, file = "titanic_clean.csv")
