762d86af9b1bd2a750f2c106ba0b47ab0426df5f
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-15 09:17:12 -0400

    3: Lifeboat

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -7,3 +7,5 @@ Data <- read_csv("titanic_original.csv")
 Data$embarked <- replace(Data$embarked, is.na(Data$embarked), "S")
 
 Data$age <- replace(Data$age, is.na(Data$age), mean(Data$age, na.rm = TRUE))
+
+Data$boat <- replace(Data$boat, is.na(Data$boat), "None")
