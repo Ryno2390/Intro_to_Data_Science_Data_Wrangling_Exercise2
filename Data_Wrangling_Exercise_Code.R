19eabcc465604bb3e6e2c53074de6ab02d5e843e
YOUR FULL NAME <SCHULTZRYNE@GMAIL.COM>
2018-10-15 09:15:03 -0400

    2: Age (Calculate the mean of the Age column and use that value to populate the missing values)

--- Data_Wrangling_Exercise_Code.R
+++ Data_Wrangling_Exercise_Code.R
@@ -5,3 +5,5 @@ library(readr)
 Data <- read_csv("titanic_original.csv")
 
 Data$embarked <- replace(Data$embarked, is.na(Data$embarked), "S")
+
+Data$age <- replace(Data$age, is.na(Data$age), mean(Data$age, na.rm = TRUE))
